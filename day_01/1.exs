1df3c7a115fb0ad8f5fcc44e4b1eccfcd79198f7
James Edward Gray II <james@graysoftinc.com>
2022-12-01 18:22:19 -0600

    Day 1

---
+++ day_01/1.exs
@@ -0,0 +1,22 @@
+IO.stream()
+|> Stream.map(&String.trim/1)
+|> Stream.chunk_while(
+  [],
+  fn line, snacks ->
+    if line == "" do
+      {:cont, Enum.reverse(snacks), []}
+    else
+      {:cont, [line | snacks]}
+    end
+  end,
+  fn snacks ->
+    {:cont, Enum.reverse(snacks), []}
+  end
+)
+|> Enum.map(fn snacks ->
+  snacks
+  |> Enum.map(&String.to_integer/1)
+  |> Enum.sum()
+end)
+|> Enum.max()
+|> IO.inspect()
